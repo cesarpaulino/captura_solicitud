05fe2ec80fdf740c923c39a44edd91b24da84967
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2025-02-26 17:52:45 -0600

    Agregar comentarios para `secretarias.php`

--- php/secretarias.php
+++ php/secretarias.php
@@ -2,12 +2,12 @@
 include 'conexion.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $secretarias_id = $_POST['secretarias'];
+    $secretarias_id = $_POST['secretarias']; // Obtener la secretaria seleccionada
 
-    if (!empty($secretarias_id)) {
-        $query = $conn->query("SELECT nombre FROM secretarias WHERE id = $secretarias_id");
+    if (!empty($secretarias_id)) { // Se necesita seleccionar una secretaria
+        $query = $conn->query("SELECT nombre FROM secretarias WHERE id = $secretarias_id"); // Consultar el nombre de la secretaria desde la base de datos
         $secretarias = $query->fetch_assoc();
-        echo "Persona que recibe la llamada: " . $secretarias['nombre'];
+        echo "Persona que recibe la llamada: " . $secretarias['nombre']; // Colocar en pantalla el nombre de quien recibe la llamada
     } else {
         echo "Por favor, seleccione una Persona que recibe la llamada.";
     }
