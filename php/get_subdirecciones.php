6f95a89b5dddda0184dc8420e8a3fba42234b5fe
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2025-02-25 19:23:47 -0600

    Agregar comentarios para `get_subdirecciones.php`

--- php/get_subdirecciones.php
+++ php/get_subdirecciones.php
@@ -1,9 +1,16 @@
 <?php
 require 'conexion.php';
 $id_direccion = $_POST['id'];
+/*
+    Ejecutar una consulta para obtener el ID y los nombres de las subdirecciones
+    de la base de datos, cuyo ID de direcciones equivalga al ID de la dirección seleccionada.
+*/
 $query = $conexion->query("SELECT id, nombre FROM subdirecciones WHERE id_direcciones = $id_direccion");
 
+// Asignar opción inicial
 $options = "<option value=''>Seleccione Subdirección</option>";
+
+// Crear opciones conforme a la información obtenida
 while ($row = $query->fetch(PDO::FETCH_ASSOC)) { // Usar PDO en lugar de fetch_assoc()
     $options .= "<option value='{$row['id']}' data-nombre='{$row['nombre']}'>{$row['nombre']}</option>";
 }
