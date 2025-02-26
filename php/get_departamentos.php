8a9ff293241e49d18f5060bd782ed5c3f0b06ad4
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2025-02-25 19:01:21 -0600

    Agregar comentarios para `get_departamentos.php`

--- php/get_departamentos.php
+++ php/get_departamentos.php
@@ -2,9 +2,14 @@
 
 require 'conexion.php';// Asegúrate de que este archivo establece $conexion como instancia de PDO
 $id_direccion = $_POST['id'];
+/*
+    Ejecutar una consulta para obtener el ID y los nombres de los departamentos
+    de la base de datos, cuyo ID de subárea equivalga al ID de la dirección seleccionada.
+*/
 $query = $conexion->query("SELECT id, nombre FROM departamentos WHERE id_subarea = $id_direccion");
-
+// Asignar la primera opción para indicar que se debe seleccionar una opción.
 $options = "<option value=''>Seleccione Subdirección</option>";
+// Crear las opciones conforme a la información obtenida
 while ($row = $query->fetch(PDO::FETCH_ASSOC)) { // Usar PDO en lugar de fetch_assoc()
     $options .= "<option value='{$row['id']}' data-nombre='{$row['nombre']}'>{$row['nombre']}</option>";
 }
