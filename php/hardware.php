ce8359c111d19c428b1d2fa1d161127dcc9f5802
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2025-02-27 19:42:34 -0600

    Agregar comentarios para `hardware.php` y `procesar.php`

--- php/hardware.php
+++ php/hardware.php
@@ -1,11 +1,11 @@
 <?php
-include 'conexion.php';
+include 'conexion.php'; // Obtener la conexión
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $hardware_id = $_POST['hardware'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") { // Si se publica
+    $hardware_id = $_POST['hardware']; // Index seleccionado del select de equipo de cómputo
 
     if (!empty($hardware_id)) {
-        $query = $conn->query("SELECT nombre FROM hardware WHERE id = $hardware_id");
+        $query = $conn->query("SELECT nombre FROM hardware WHERE id = $hardware_id"); // Obtener el nombre del hardware seleccionado
         $hardware = $query->fetch_assoc();
         echo "Persona que recibe la llamada: " . $hardware['nombre'];
     } else {
--- php/procesar.php
+++ php/procesar.php
@@ -1,13 +1,13 @@
 <?php
-include 'conexion.php';
+include 'conexion.php'; 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {  // Si se publica
     $direcciones_generales_id = $_POST['direcciones_generales'];
 
-    if (!empty($direcciones_generales_id)) {
-        $query = $conn->query("SELECT nombre FROM direcciones_generales WHERE id = $direcciones_generales_id");
+    if (!empty($direcciones_generales_id)) { // Se debe seleccionar al menos una dirección general
+        $query = $conn->query("SELECT nombre FROM direcciones_generales WHERE id = $direcciones_generales_id"); // Obtener el nombre de la dirección general
         $direcciones_generales = $query->fetch_assoc();
-        echo "Direcciones Generales: " . $direcciones_generalesa['nombre'];
+        echo "Direcciones Generales: " . $direcciones_generalesa['nombre']; // Mostrar nombre de la dirección general
     } else {
         echo "Por favor, seleccione una Direcciones Generales.";
     }
